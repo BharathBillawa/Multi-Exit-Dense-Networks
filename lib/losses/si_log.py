dc255798648813856b0ea48de1d31a13bc3b3948
Bharath V Poojary <bharathbillawa@gmail.com>
2022-05-07 19:02:15 +0200

    Add custom loss functions

---
+++ lib/losses/si_log.py
@@ -0,0 +1,27 @@
+"""
+Inspired from: https://github.com/shariqfarooq123/AdaBins
+"""
+
+import torch
+import torch.nn as nn
+
+
+class SILogLoss(nn.Module):  # Main loss function used in AdaBins paper
+    def __init__(self):
+        super(SILogLoss, self).__init__()
+        self.name = 'SILog'
+
+    def forward(self, input, target, mask = None, interpolate = True):
+        if interpolate:
+            input = nn.functional.interpolate(input, target.shape[-2:], mode='bilinear', align_corners=True)
+
+        if mask is not None:
+            input = input[mask]
+            target = target[mask]
+        g = torch.log(input) - torch.log(target)
+        # n, c, h, w = g.shape
+        # norm = 1/(h*w)
+        # Dg = norm * torch.sum(g**2) - (0.85/(norm**2)) * (torch.sum(g))**2
+
+        Dg = torch.var(g) + 0.15 * torch.pow(torch.mean(g), 2)
+        return 10 * torch.sqrt(Dg)
